4388997d59886af1a37ae55c0701cd7f969106e3
MVandaglas <martijn.wissink@vandaglas.com>
2025-11-10 13:13:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -173,7 +173,7 @@ def df_to_simple_pdf(df: pd.DataFrame, title: str = "Prijslijst") -> bytes:
         show_df["Omzet conditie"] = (show_df["Omzet totaal"] * np.random.uniform(0, 1, size=len(show_df))).round(2)
         
         # Effect aanpassing = Omzet conditie * (Handmatige prijs / Huidige m2 prijs) - Omzet conditie
-        show_df["Effect aanpassing"] = ((show_df["Omzet conditie"] / pd.to_numeric(show_df["Huidige m2 prijs"]) * pd.to_numeric(show_df["Handmatige prijs"] - show_df["Omzet conditie"])
+        show_df["Effect aanpassing"] = (show_df["Omzet conditie"] / pd.to_numeric(show_df["Huidige m2 prijs"]) * pd.to_numeric(show_df["Handmatige prijs"] - show_df["Omzet conditie"])
         
         x0, y0 = 1.2*cm, height - 3*cm
         line_h = 0.6*cm
